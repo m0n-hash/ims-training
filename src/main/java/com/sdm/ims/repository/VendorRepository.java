0c91041f7b451959250f0c24349ae63cc1e314ba
khinmyatkyi <khinmyatkyi93@gmail.com>
2020-07-27 21:36:21 +0630

    Add files via upload

---
+++ src/main/java/com/sdm/ims/repository/VendorRepository.java
@@ -0,0 +1,13 @@
+package com.sdm.ims.repository;
+
+import com.sdm.ims.entity.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor,Integer> {
+
+
+}
